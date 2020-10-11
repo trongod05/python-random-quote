708ed40b824a9fc0a2cfb21b937992805f5bb3e5
trongod05 <trongod@gmail.com>
2020-10-10 20:17:27 -0500

    Accept user input, read file, write file, print file.

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,29 @@
 import random
 
 def primary():
-  print("Keep it logically awesome.")
+  add_response = input("Would you like to add quotes to file: ")
+  print(add_response)
+
+  while (add_response == 'Y' or add_response == 'y'):
+  	quote = input("Type quote: ")
+  	f = open("quotes.txt", "a")
+  	f.writelines(quote + "\n")
+  	f.close()
+  	add_response = input("Would you like to add more quotes to file: ")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  print("Keep it logically awesome.")
+  last = len(quotes) - 1
+  
+  x = 0
+  while x < last:
+  	rnd = random.randint(0, last)
+  	print(quotes[rnd], end = '')
+  	x = x + 1
+
 
 if __name__== "__main__":
   primary()
